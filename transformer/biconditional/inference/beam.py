5c9272b87ca01cddb9c1997d18e89578b75cf0a6
naripok <fernando.canteruccio@gmail.com>
2020-04-14 08:02:42 -0300

    expose alpha on predict

--- transformer/biconditional/inference/beam.py
+++ transformer/biconditional/inference/beam.py
@@ -7,7 +7,8 @@ from ...components import Beam
 logging.basicConfig(level=logging.INFO)
 
 
-def evaluate_beam(tokenizer, model, sentence, context, max_length, beam_size, max_hypothesis, max_depth, training):
+def evaluate_beam(tokenizer, model, sentence, context, max_length, beam_size, max_hypothesis,
+        max_depth, alpha, training):
     start_token, end_token = [tokenizer.vocab_size], [tokenizer.vocab_size + 1]
 
     sentence = preprocess_sentence(sentence)
@@ -17,16 +18,16 @@ def evaluate_beam(tokenizer, model, sentence, context, max_length, beam_size, ma
 
     beam = Beam(model, start_token, end_token, max_length, beam_size)
 
-    predictions = beam.run([sentence, context], max_hypothesis, max_depth)
+    predictions = beam.run([sentence, context], max_hypothesis, max_depth, alpha)
 
     return predictions
 
 
 def predict_beam(tokenizer, model, sentence, context, max_length=None, beam_size=8, max_hypothesis=4,
-        max_depth=32, training=False, show_hypotheses=True):
+        max_depth=32, alpha=0.7, training=False, show_hypotheses=True):
 
     predictions = evaluate_beam(tokenizer, model, sentence, context, max_length, beam_size, max_hypothesis,
-            max_depth, training)
+            max_depth, alpha, training)
 
     if show_hypotheses:
         for prediction in predictions:
